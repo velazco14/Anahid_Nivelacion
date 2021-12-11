7e1d95e04065c73aaa171f64fd4696d5afedd9d0
velazco14 <91619131+velazco14@users.noreply.github.com>
2021-12-10 21:56:15 -0600

    Update main.js

--- api/routes/main.js
+++ api/routes/main.js
@@ -8,72 +8,48 @@ var urlencodedParser = bodyParser.urlencoded({ extended: true });
 var productos=[
   {
       "id":1,
-      "image":"mascarilla1.jpg",
-      "precio":20,
-      "title":"MASCARILLA DE CARBÓN"
-  },
-  {
-      "id":2,
-      "image":"mascarilla2.jpg",
-      "precio":35,
-      "title":"MASCARILLA DE LIMÓN"
-  },
-  {
-      "id":3,
-      "image":"mascarilla3.jpg",
-      "precio":15.50,
-      "title":"MASCARILLA HIDRATANTE"
-  },
-  {
-      "id":4,
-      "image":"mascarilla4.jpg",
-      "precio":20.20,
-      "title":"MASCARILLA DE GRANADA"
-  },
-  {
-      "id":5,
       "image":"micelar1.jpg",
       "precio":19,
       "title":"AGUA MICELAR"
   },
   {
-      "id":6,
+      "id":2,
       "image":"micelar2.jpg",
       "precio":45,
       "title":"AGUA MICELAR EN ACEITE"
   },
   {
-      "id":7,
+      "id":3,
       "image":"micelar3.jpg",
       "precio":23.99,
       "title":"AGUA MICELAR PURE ACTIVE"
   },
   {
-      "id":8,
+      "id":4,
       "image":"micelar4.jpg",
       "precio":50,
       "title":"AGUA MICELAR SENSITIVE"
   },
   {
-      "id":9,
+      "id":5,
       "image":"men1.png",
       "precio":16,
       "title":"ACNO FIGHT LIMPIADOR"
   },
   {
-      "id":10,
+      "id":6,
       "image":"men2.png",
       "precio":17.50,
       "title":"ACNO FIGHT EXFOLIANTE"
   },
   {
-      "id":11,
+      "id":7,
       "image":"men3.png",
       "precio":45,
       "title":"OIL CONTROL LIMPIADOR"
   },
   {
-      "id":12,
+      "id":8,
       "image":"men4.png",
       "precio":45,
       "title":"OIL CONTROL EXFOLIANTE"
